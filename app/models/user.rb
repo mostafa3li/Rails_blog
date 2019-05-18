a5820de15493c1beec46ca3da951909e708274ff
mostafa <mostapha.a.aly@gmail.com>
2019-05-18 15:02:20 +0200

    blog webApp

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # before_action :authenticate_user!
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates_uniqueness_of :user_name
+
+end
